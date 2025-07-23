6031659c5af507bcaa5aeb2bb7d59e15babbcb76
harmel3056 <melh1988@hotmail.com>
2025-07-23 01:07:40 +0000

    first loops

--- _printf.c
+++ _printf.c
@@ -8,10 +8,26 @@
 
 int _printf(const char *format, ...)
 {
-	int ;
-	int;
+	int i = 0;
+	int j;
 
-	if (format == NULL)
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+			j = i + 1;
+
+			if (format[i] == '%' && format[j] == 's')
+				
+
+
+			write(1, &format[i], strlen(format[i]));  //check strlen necessity here, check later
+			i++;
+
+
+
+
+
 		return (-1);
 
 }
