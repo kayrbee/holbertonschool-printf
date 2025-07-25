b6f8fa1cd36496850f15e622d5d14fced6744771
Katherine Beringer <github@katbee.au>
2025-07-25 07:09:09 +0000

    Fix the tests

--- tests/main.c
+++ tests/main.c
@@ -5,21 +5,21 @@ int main(void)
 
         str = "test";
 
-        printf("Test 1: string literal ");
-        _printf("Apple\n");
-        printf("Test 2: %%c without nl");
-        _printf("%c", a);
-        printf("Test 3: %%c with nl");
-        _printf("%c\n", a);
-        printf("Test 4: %%s ");
-        _printf("%s\n", str);
-        printf("Test 5: %%%% ");
-        _printf("%%");
-        printf("Test 6: combo input with %%%% ");
-        _printf("str %%\n");
+        printf("Test 1: string literal\n");
+        printf("Test 2: %%c without nl\n");
+        printf("Test 3: %%c with nl\n");
+        printf("Test 4: %%s\n");
+        printf("Test 5: %%%%\n");
+        printf("Test 6: combo input with %%%%\n");
         printf("Test 7: error checks - pass in null\n");
+        printf("Test 8: error checks - pass in a single %%\n");
+        _printf("Apple\n");
+        _printf("%c", a);
+        _printf("%c\n", a);
+        _printf("%s\n", str);
+        _printf("%%");
+        _printf("str %%\n");
         _printf(NULL);
-        printf("Test 8: error checks - pass in a single %%");
         _printf("%");
 
         return (0);
