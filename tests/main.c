e0679f665a4fdec841b445f0bc751a2feb07e47d
Katherine Beringer <github@katbee.au>
2025-07-25 07:06:09 +0000

    Fix printf statements

--- tests/main.c
+++ tests/main.c
@@ -6,21 +6,21 @@ int main(void)
         str = "test";
 
         printf("Test 1: string literal ");
-	_printf("Apple\n");
-	printf("Test 2: \%c without nl");
+        _printf("Apple\n");
+        printf("Test 2: %%c without nl");
         _printf("%c", a);
-	printf("Test 3: \%c with nl");
+        printf("Test 3: %%c with nl");
         _printf("%c\n", a);
-	printf("Test 4: \%s ");
+        printf("Test 4: %%s ");
         _printf("%s\n", str);
-	printf("Test 5: \%\% ");
+        printf("Test 5: %%%% ");
         _printf("%%");
-	printf("Test 6: combo input with \%\% ");
+        printf("Test 6: combo input with %%%% ");
         _printf("str %%\n");
-	printf("Test 7: error checks - pass in null\n");
-	_printf(NULL);
-	printf("Test 8: error checks - pass in a single \%");
-	_printf("%");
+        printf("Test 7: error checks - pass in null\n");
+        _printf(NULL);
+        printf("Test 8: error checks - pass in a single %%");
+        _printf("%");
 
         return (0);
 }
